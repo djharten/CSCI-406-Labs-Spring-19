fdda3ea2a8bfebac5fbf6027f3622279e81f0f97
djharten <djharten@millersville.edu>
2019-03-21 19:22:45 -0400

    Updating index.js in app_api

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var ctrlBlog = require('../controllers/blog');
 
 /* GET, POST, PUT and DELETE API routes for blog pages */
-router.get('/blog', ctrlBlog.blogReadAll); // this is blogList
-router.post('/blog', ctrlBlog.blogAddOne); // this is addOne
-router.get('/blog/:blogid', ctrlBlog.blogReadOne); // this is readOne
-router.put('/blog/:blogid', ctrlBlog.blogEditOne); // this is editOne
-router.delete('/blog/:blogid', ctrlBlog.blogDeleteOne); // this is deleteOne
+router.get('/blog', ctrlBlog.blogList); // this is blogList
+router.post('/blog', ctrlBlog.addOne); // this is addOne
+router.get('/blog/:blogid', ctrlBlog.readOne); // this is readOne
+router.put('/blog/:blogid', ctrlBlog.editOne); // this is editOne
+router.delete('/blog/:blogid', ctrlBlog.deleteOne); // this is deleteOne
 
 module.exports = router;
