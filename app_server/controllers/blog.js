e98dbda79306996596ba792bf7a0990c43990b0e
djharten <djharten@millersville.edu>
2019-03-29 15:48:52 -0400

    update

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var apiOptions = { server: 'http://localhost:3000' };
+var apiOptions = { server: 'http://18.208.205.232' };
 var path, requestOptions;
 
 /* GET: Lists all blog pages */
